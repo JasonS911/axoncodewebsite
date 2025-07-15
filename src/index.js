ba69e3fd38ebdccd79694dcfa4d7690b9db236e2
Jason <shij1245@gmail.com>
2025-07-15 14:00:14 -0400

    test

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
